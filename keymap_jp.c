a90f43bd5c8485046c9b81798fcb7a43de4bec78
Phong Nguyen <nhphong1406@gmail.com>
2018-08-05 00:47:35 +0700

    Uses dual roles for some keys

--- keymap_jp.c
+++ keymap_jp.c
@@ -20,16 +20,16 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     * |-----------------------------------------------------------|
     * |Shft   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  \| Up|Sft|
     * |-----------------------------------------------------------|
-    * |   ||Ctl|Alt|Cmd|   |   Spc   |Bsp|   |   |   ||Lft|Dwn|Rgh|
+    * |   ||Ctl|Alt|Cmd|Ent|   Spc   |Bsp|   |   |   ||Lft|Dwn|Rgh|
     * `-----------------------------------------------------------'
     */
 
     KEYMAP_JP( \
         ESC,  1,   2,    3,    4,   5,   6,   7,    8,    9,    0,    MINS, EQL,  BSLS, BSPC, \
         TAB,  Q,   W,    E,    R,   T,   Y,   U,    I,    O,    P,    GRV,  LBRC,             \
-        LCTL, A,   S,    D,    F,   G,   H,   J,    K,    L,    FN2,  QUOT, RBRC, ENT,        \
-        LSFT, Z,   X,    C,    V,   B,   N,   M,    COMM, DOT,  SLSH, RO,   UP,   RSFT,       \
-        FN0,  FN1, LALT, LGUI, ENT,    SPC,   BSPC, RGUI, RALT, FN0,  LEFT, DOWN, RGHT        \
+        LCTL, A,   S,    D,    F,   G,   H,   J,    K,    L,    FN4,  FN5,  RBRC, ENT,        \
+        LSFT, Z,   X,    C,    V,   B,   N,   M,    COMM, DOT,  FN6,  FN7,  UP,   RSFT,       \
+        FN0,  FN1, LALT, LGUI, FN2,    SPC,   FN3,  RGUI, RALT, FN0,  LEFT, DOWN, RGHT        \
     ),
 
     /* Layer 1: HHKB mode (HHKB Fn)
@@ -56,7 +56,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 
     /* Layer 2: Mouse
     * ,-----------------------------------------------------------.
-    * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+    * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |   |
     * |-----------------------------------------------------------|
     * |     |   |Mcl|MU |Rcl|   |   |Rcl|WU |Mcl|   |   |   |     |
     * |------------------------------------------------------`    |
@@ -69,7 +69,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     */
 
     KEYMAP_JP(
-        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+        TRNS, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS, \
         TRNS, TRNS, BTN3, MS_U, BTN2, TRNS, TRNS, BTN2, WH_U, BTN3, TRNS, TRNS, TRNS,             \
         TRNS, TRNS, MS_L, MS_D, MS_R, BTN1, BTN1, WH_L, WH_D, WH_R, TRNS, TRNS, TRNS, TRNS,       \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       \
@@ -108,7 +108,12 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 #else
 const action_t fn_actions[] PROGMEM = {
 #endif
-    [0] = ACTION_LAYER_TAP_KEY(1, KC_GRV),   // ` ~
-    [1] = ACTION_MODS_KEY(MOD_LCTL, KC_GRV), // ` ~
-    [2] = ACTION_LAYER_TAP_KEY(2, KC_SCLN),  // ;
+    [0] = ACTION_LAYER_TAP_KEY(1, KC_GRV),    // ` ~
+    [1] = ACTION_MODS_KEY(MOD_LCTL, KC_GRV),  // ` ~
+    [2] = ACTION_LAYER_TAP_KEY(2, KC_ENT),    // Enter
+    [3] = ACTION_LAYER_TAP_KEY(2, KC_BSPC),   // Backspace
+    [4] = ACTION_LAYER_TAP_KEY(2, KC_SCLN),   // ; :
+    [5] = ACTION_MODS_KEY(MOD_RGUI, KC_QUOT), // ' "
+    [6] = ACTION_MODS_KEY(MOD_RCTL, KC_SLSH), // / ?
+    [7] = ACTION_MODS_KEY(MOD_RSFT, KC_BSLS), // \ |
 };
